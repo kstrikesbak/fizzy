6db687b63902cbc7e91b0c611d02ee7ab592ea51
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-01 15:16:53 -0400

    complete

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 function fizzy(num) {
 
+if (num % 3 === 0 && !(num % 5===0)) {
+    return 'Fizz';
+} else if (num % 5 ===0 && !(num % 3===0)) {
+  return 'Buzz';
+} else if (num % 15 === 0) {
+  return 'FizzBuzz';
+} else {
+    return num;
+  }
 }
+
+console.log(fizzy(6));
 
 module.exports = {
   fizzy,
 }
+
+
+
